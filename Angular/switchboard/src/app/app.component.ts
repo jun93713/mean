cc22b968a778c91d3ef05328a84dea35ebc6a5c9
jun93713 <jun93713@hotmail.com>
2017-07-17 11:06:25 -0700

    finished switchboard

--- Angular/switchboard/src/app/app.component.ts
+++ Angular/switchboard/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Switchboard';
+  many = [false, false, false, false, false, false, false, false, false, false]
 }
