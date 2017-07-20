955c2c7aef58b0b4540b8e57aae43ae15200c82c
jun93713 <jun93713@hotmail.com>
2017-07-19 21:37:56 -0700

    GitHub Score -- unfinished

--- Angular/github-score/src/app/app.component.ts
+++ Angular/github-score/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { HttpService } from './http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  username = null;
+
+  constructor(private _httpService: HttpService){}
 }
--- Angular/github-score/src/app/app.module.ts
+++ Angular/github-score/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpService } from './http.service';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
